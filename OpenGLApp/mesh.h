69111118e5694a6f62a2bd9a9d8d14b957ee4b5c
salmarto <salvatoremartoranaa@gmail.com>
2026-01-04 16:39:43 +0100

    Improve model loading preloading materials

--- OpenGLApp/mesh.h
+++ OpenGLApp/mesh.h
@@ -2,7 +2,6 @@
 
 #include "texture.h"
 #include "shader.h"
-
 #include <vector>
 #include <glad/glad.h>
 #include <glm/glm.hpp>
@@ -14,48 +13,55 @@ struct Vertex {
     glm::vec3 tangent;
 };
 
+struct Material {
+    std::string name;
+    Texture *textures[Texture::N_TYPES] = {};
+
+    void apply(const Shader &shader) const {
+        const char *samplers[] = {"diffuseMap", "ambientOcclusionMap", "normalMap", "roughnessMap", "metalnessMap"};
+        const char *flags[] = {
+            "hasDiffuseMap", "hasAmbientOcclusionMap", "hasNormalMap", "hasRoughnessMap", "hasMetalnessMap"
+        };
+
+        for (int unit = 0; unit < Texture::N_TYPES; unit++) {
+            if (textures[unit]) {
+                textures[unit]->use(unit);
+                shader.setInt(samplers[unit], unit);
+                shader.setInt(flags[unit], true);
+            } else {
+                shader.setInt(flags[unit], false);
+            }
+        }
+    }
+};
+
 class Mesh {
-    unsigned int VAO;
-    unsigned int VBO;
-    unsigned int EBO;
+    unsigned int VAO, VBO, EBO;
+
 public:
-    enum Type { // Used for special cases
-        Default,
-        Collision,
-        Button,
-        Light,
-        Portal
-    } type;
+    enum Type { Default, Collision, Button, Light, Portal } type;
 
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
-    Texture *textures[Texture::N_TYPES] = {}; // Each mesh can use only one texture per type
-
+    Material *material;
     std::string name;
 
-    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::string name = "", Type type = Default) :
-            vertices(std::move(vertices)),
-            indices(std::move(indices)),
-            name(std::move(name)),
-            type(type) {
+    Mesh(std::vector<Vertex> v, std::vector<unsigned int> i, Material *mat, std::string n = "",
+         Type t = Default) : vertices(std::move(v)), indices(std::move(i)), material(mat), name(std::move(n)), type(t) {
         init();
     }
 
     void free() const {
-        if (VAO) glDeleteVertexArrays(1, &VAO);
-        if (VBO) glDeleteBuffers(1, &VBO);
-        if (EBO) glDeleteBuffers(1, &EBO);
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
     }
 
-    // Render the mesh skip textures settings for the picking shader
-    void draw(Shader &shader, bool useTextures = true) const {
-        if (useTextures) applyTextures(shader);
+    void draw(Shader &shader, bool useMaterial = true) const {
+        if (useMaterial && material) material->apply(shader);
 
-        // Draw mesh
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
-
-        // Reset to default
         glBindVertexArray(0);
         glActiveTexture(GL_TEXTURE0);
     }
@@ -73,47 +79,14 @@ private:
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
-        // Vertex positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) 0);
-        // Vertex normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, normal));
-        // Vertex texture coords
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, texCoords));
-        // Vertex tangent
         glEnableVertexAttribArray(3);
         glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, tangent));
-
         glBindVertexArray(0);
-    }
-
-    void applyTextures(const Shader &shader) const {
-        const char* samplers[] = {
-            "diffuseMap",
-            "ambientOcclusionMap",
-            "normalMap",
-            "roughnessMap",
-            "metalnessMap",
-        };
-        const char* flags[] = {
-            "hasDiffuseMap",
-            "hasAmbientOcclusionMap",
-            "hasNormalMap",
-            "hasRoughnessMap",
-            "hasMetalnessMap",
-        };
-
-        // For simplicity each texture unit is bound to the corresponding type enum
-        for (int unit = 0; unit < Texture::N_TYPES; unit++) {
-            if (textures[unit]) {
-                textures[unit]->use(unit);
-                shader.setInt(samplers[unit], unit);
-                shader.setInt(flags[unit], true);
-            } else {
-                shader.setInt(flags[unit], false);
-            }
-        }
     }
 };
--- OpenGLApp/model.h
+++ OpenGLApp/model.h
@@ -2,155 +2,116 @@
 
 #include "utils.h"
 #include "mesh.h"
-
 #include <vector>
-#include <glm/glm.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
 class Model {
     IAssetManager *assetManager;
+    std::vector<Material> materials;
 
 public:
     std::vector<Mesh> meshes;
     std::vector<AABB> collisions;
     std::string path;
 
-    Model(IAssetManager *am, const std::string &path = "")
-        : assetManager(am), path(getResourcePath(path)) {
+    Model(IAssetManager *am, const std::string &p = "") : assetManager(am), path(getResourcePath(p)) {
     }
 
-    // Loads a model using ASSIMP
     bool load() {
         Assimp::Importer importer;
-        const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals |
-                                                       aiProcess_CalcTangentSpace);
+        const aiScene *scene = importer.ReadFile(
+            path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_CalcTangentSpace);
 
         if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
             std::cerr << "ERROR: ASSIMP: " << importer.GetErrorString() << std::endl;
             return false;
         }
 
+        // Preload all materials
+        for (unsigned int i = 0; i < scene->mNumMaterials; i++) {
+            Material mat;
+            mat.name = scene->mMaterials[i]->GetName().C_Str();
+            loadMaterialTextures(scene->mMaterials[i], mat);
+            materials.push_back(mat);
+        }
+
+        // Process mesh hierarchy
         processNode(scene->mRootNode, scene);
         return true;
     }
 
-    // Free GPU resources
-    void free() {
-        for (auto &mesh: meshes) mesh.free();
-    }
+    void free() { for (auto &mesh: meshes) mesh.free(); }
 
-    // Draws the model by iterating through all its meshes
     void draw(Shader &shader, bool useTextures = true) const {
-        for (auto &mesh: meshes) mesh.draw(shader, useTextures);
+        for (const auto &mesh: meshes) mesh.draw(shader, useTextures);
     }
 
 private:
-    // Recursively processes nodes
     void processNode(aiNode *node, const aiScene *scene) {
         for (unsigned int i = 0; i < node->mNumMeshes; i++) {
-            aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
-            processMesh(mesh, scene);
+            processMesh(scene->mMeshes[node->mMeshes[i]]);
         }
-
         for (unsigned int i = 0; i < node->mNumChildren; i++) processNode(node->mChildren[i], scene);
     }
 
-    void processMesh(aiMesh *mesh, const aiScene *scene) {
+    void processMesh(aiMesh *mesh) {
         std::vector<Vertex> vertices;
         std::vector<unsigned int> indices;
+        glm::vec3 min(FLT_MAX), max(-FLT_MAX);
 
-        // Used for bounding boxes
-        glm::vec3 min(std::numeric_limits<float>::max());
-        glm::vec3 max(-std::numeric_limits<float>::max());
-
-        // Process vertices
         for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
-            Vertex vertex = {};
-            vertex.position = {mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z};
+            Vertex v = {};
+            v.position = {mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z};
 
-            if (min.x > vertex.position.x) min.x = vertex.position.x;
-            if (min.y > vertex.position.y) min.y = vertex.position.y;
-            if (min.z > vertex.position.z) min.z = vertex.position.z;
+            min = glm::min(min, v.position);
+            max = glm::max(max, v.position);
 
-            if (max.x < vertex.position.x) max.x = vertex.position.x;
-            if (max.y < vertex.position.y) max.y = vertex.position.y;
-            if (max.z < vertex.position.z) max.z = vertex.position.z;
-
-            if (mesh->HasNormals())
-                vertex.normal = {mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z};
-
+            if (mesh->HasNormals()) v.normal = {mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z};
             if (mesh->mTextureCoords[0]) {
-                vertex.texCoords = {mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y};
-                vertex.tangent = {mesh->mTangents[i].x, mesh->mTangents[i].y, mesh->mTangents[i].z};
-            } else {
-                // Fallback values
-                vertex.texCoords = glm::vec2(0.0f, 0.0f);
-                vertex.tangent = glm::vec3(1.0f, 0.0f, 0.0f);
+                v.texCoords = {mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y};
+                v.tangent = {mesh->mTangents[i].x, mesh->mTangents[i].y, mesh->mTangents[i].z};
             }
-            vertices.push_back(vertex);
+            vertices.push_back(v);
         }
 
-        // Process indices
         for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
-            aiFace face = mesh->mFaces[i];
-            for (unsigned int j = 0; j < face.mNumIndices; j++)
-                indices.push_back(face.mIndices[j]);
+            for (unsigned int j = 0; j < mesh->mFaces[i].mNumIndices; j++)
+                indices.push_back(mesh->mFaces[i].mIndices[j]);
         }
 
-        size_t pos;
-        std::string num = "0";
-        Mesh::Type type = Mesh::Default;
-
-        // Convert to lowercase for easier matching
         std::string name = mesh->mName.C_Str();
         std::string nameLow = StringToLower(name);
+        Mesh::Type type = Mesh::Default;
 
-        // TODO: Assign the proper portal texture and light material.
         if (nameLow.find("collision") != std::string::npos) {
             collisions.emplace_back(AABB{min, max});
-        } else if ((pos = nameLow.find("volume")) != std::string::npos ||
-                   (pos = nameLow.find("glass")) != std::string::npos) {
-            // TODO: implement separate shaders, for now just ignore the mesh.
-        } else if ((pos = nameLow.find("button")) != std::string::npos) {
-            name = name.substr(0, pos);
-            type = Mesh::Button;
-        } else if ((pos = nameLow.find("light")) != std::string::npos) {
-            name = name.substr(0, pos);
-            type = Mesh::Light;
-        } else if ((pos = nameLow.find("portal")) != std::string::npos) {
-            name = name.substr(0, pos);
-            type = Mesh::Portal;
-        } else {
-            // Process material textures
-            aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
-
-            Mesh newMesh(vertices, indices, name, type);
-            loadMaterialTextures(material, newMesh);
-            meshes.emplace_back(std::move(newMesh));
+            return;
         }
+
+        // TODO: Handle the different cases.
+        if (nameLow.find("button") != std::string::npos) type = Mesh::Button;
+        else if (nameLow.find("light") != std::string::npos) type = Mesh::Light;
+        else if (nameLow.find("portal") != std::string::npos) type = Mesh::Portal;
+
+        // Assign correct preloaded material pointer
+        Material *matPtr = (mesh->mMaterialIndex < materials.size()) ? &materials[mesh->mMaterialIndex] : nullptr;
+        meshes.emplace_back(vertices, indices, matPtr, name, type);
     }
 
-    void loadMaterialTextures(aiMaterial *mat, Mesh &mesh) {
+    void loadMaterialTextures(aiMaterial *mat, Material &material) {
         aiTextureType types[] = {
-            aiTextureType_DIFFUSE,
-            aiTextureType_SPECULAR,
-            aiTextureType_AMBIENT,
-            aiTextureType_HEIGHT, // Often used for Normals
-            aiTextureType_NORMALS,
-            aiTextureType_SHININESS, // Roughness
-            aiTextureType_REFLECTION // Metalness
+            aiTextureType_DIFFUSE, aiTextureType_SPECULAR, aiTextureType_AMBIENT,
+            aiTextureType_HEIGHT, aiTextureType_NORMALS, aiTextureType_SHININESS, aiTextureType_REFLECTION
         };
 
         for (const auto type: types) {
             for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
-                aiString name;
-                mat->GetTexture(type, i, &name);
-
-                Texture *tex = assetManager->getTexture(name.C_Str());
-
-                if (tex) mesh.textures[tex->type] = tex;
+                aiString str;
+                mat->GetTexture(type, i, &str);
+                Texture *tex = assetManager->getTexture(str.C_Str());
+                if (tex) material.textures[tex->type] = tex; // Maps Assimp texture to internal type
             }
         }
     }
